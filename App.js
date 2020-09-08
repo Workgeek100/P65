be85a06e251d9b4b72ca8751d8e9e9a76cda6006
Workgeek100 <maybha_70@hotmail.com>
2020-09-08 14:58:38 +0300

    P65

---
+++ App.js
@@ -0,0 +1,97 @@
+import * as React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Alert
+} from 'react-native';
+import { Header } from 'react-native-elements';
+import db from './localdb';
+import PhonicSoundButton from './components/PhonicSoundButton';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      chunks: [],
+      phonicSounds: [],
+    };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header
+          backgroundColor={'red'}
+          centerComponent={{
+            text: 'Text-Speech',
+            style: { color: '#fff', fontSize: 30 },
+          }}
+        />
+
+        <TextInput
+          style={styles.inputBox}
+          onChangeText={text => {
+            this.setState({ text: text });
+          }}
+          value={this.state.text}
+        />
+        <TouchableOpacity
+          style={styles.goButton}
+          onPress={() => {
+            var word = this.state.text.toLowerCase().trim();
+            db[word]?(
+            this.setState({ chunks: db[word].chunks }),
+            this.setState({ phonicSounds: db[word].phones })
+            ):
+            Alert.alert("The word does not exist in our database");
+          }}>
+          <Text style={styles.buttonText}>LISTEN</Text>
+        </TouchableOpacity>
+        <View>
+          {this.state.chunks.map((item, index) => {
+            return (
+              <PhonicSoundButton
+                wordChunk={this.state.chunks[index]}
+                soundChunk={this.state.phonicSounds[index]}
+                buttonIndex={index}
+              />
+            );
+          })}
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+  },
+  inputBox: {
+    backgroundColor:"orange",
+    marginTop: 50,
+    width: '80%',
+    alignSelf: 'center',
+    height: 40,
+    textAlign: 'center',
+    borderWidth: 4
+  },
+  goButton: {
+    width: '50%',
+    height: 55,
+    alignSelf: 'center',
+    padding: 10,
+    margin: 10,
+  },
+  buttonText: {
+    color:"red",
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
+});
